1f7b146c23cad579e2de0602d8557004bc7de12c
gracepinkie <gracepinkie90@gmail.com>
2022-06-07 11:44:06 +0200

    first commit

---
+++ script/code.js
@@ -0,0 +1,10 @@
+
+//arrays
+let bestfriends = ['Sheldon Braddley Cooper' , 'Leornad Leopard Hosfstater' , 'Penny Hofstater', 'Howard Wolowitz' , 'Rajesh Kootrtapall' , ' Amy Farrah Fowler ' , ' Bernadette    Wolowitz ' , 'Wil Wheaton' , 'Stuart Bloom '];
+console.log(bestfriends.length);
+//index/position
+console.log(bestfriends[0]);
+let middleIndex = bestfriends.length /2
+console.log(middleIndex);
+console.log(bestfriends[middleIndex])
+
